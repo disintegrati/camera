a2d16f00a5530b8107fda822155729224e78cd91
mediaintegrati <mediaintegrati@gmail.com>
2017-11-13 14:06:34 +0000

    blink durante upload

--- pulsante.py
+++ pulsante.py
@@ -2,16 +2,42 @@
 import RPi.GPIO as GPIO
 import time
 import os
+LedPin = 4
 
 GPIO.setmode(GPIO.BCM)
+GPIO.setup(LedPin, GPIO.OUT)
+GPIO.output(LedPin, GPIO.HIGH)
+
 GPIO.setup(18,GPIO.IN,pull_up_down=GPIO.PUD_UP)
+
+
+def blink():
+        t_end = time.time() + 15
+        while time.time() < t_end:
+                        GPIO.output(LedPin, GPIO.LOW)
+                        time.sleep(0.3)
+                        GPIO.output(LedPin, GPIO.HIGH)
+                        time.sleep(0.3)
+
+def destroy():
+        GPIO.output(LedPin, GPIO.LOW)
+        GPIO.cleanup()
+
 while True: 
+	GPIO.setmode(GPIO.BCM)
 	inputValue = GPIO.input(18)
 	if(inputValue == False):
 		print("Bottone premuto")
-		os.system("sh /home/pi/Desktop/test/video.sh")
+		os.system("sh /home/pi/Desktop/camera//video.sh")
+		blink()
+#		destroy()
 	time.sleep(0.3)
 
-
+ 
+#if __name__ == '__main__':
+#	try:
+#		blink()
+#	except KeyboardInterrupt:
+#		destroy()
 
 
--- video.sh
+++ video.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 today=`date '+%Y_%m_%d__%H_%M_%S'`;
 ffmpeg -thread_queue_size 2048 -f v4l2 -i /dev/video0 -t 00:00:15 /home/pi/Desktop/camera/video/$today.mp4
-####
