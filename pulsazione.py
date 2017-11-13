aefa84593de3d4df13ac97a3ed78408455735cf3
mediaintegrati <mediaintegrati@gmail.com>
2017-11-13 15:35:24 +0000

    Pulsante OK!

--- pulsazione.py
+++ pulsazione.py
@@ -2,11 +2,11 @@
 import RPi.GPIO as GPIO
 import time
 import os
-LedPin = 4
+LedPin = 6
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(LedPin, GPIO.OUT)
-GPIO.output(LedPin, GPIO.LOW)
+GPIO.output(LedPin, GPIO.HIGH)
 
 GPIO.setup(18,GPIO.IN,pull_up_down=GPIO.PUD_UP)
 
@@ -15,30 +15,27 @@ def blink():
         t_end = time.time() + 15
         while time.time() < t_end:
                         GPIO.output(LedPin, GPIO.LOW)
-                        time.sleep(0.3)
+                        time.sleep(0.1)
                         GPIO.output(LedPin, GPIO.HIGH)
-                        time.sleep(0.3)
-			GPIO.output(LedPin, GPIO.LOW)
-#	destroy()
+                        time.sleep(0.1)
+
 def destroy():
         GPIO.output(LedPin, GPIO.LOW)
         GPIO.cleanup()
 
 while True: 
-	GPIO.setmode(GPIO.BCM)
+#	GPIO.setmode(GPIO.BCM)
 	inputValue = GPIO.input(18)
 	if(inputValue == False):
 		print("Bottone premuto")
-		os.system("sh /home/pi/Desktop/camera//video.sh")
 		blink()
 #		destroy()
-	time.sleep(0.3)
-
  
 #if __name__ == '__main__':
 #	try:
 #		blink()
 #	except KeyboardInterrupt:
 #		destroy()
+
 
 
